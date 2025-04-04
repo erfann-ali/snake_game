d4f25df37b08c3ba97496ffb48745172441df4df
Erfan <erfan0503@gmail.com>
2025-04-03 23:03:56 -0400

    Improving snake game by adding high score variable and using File to save player's high score, so next time the player can see what was their high score

--- scoreboard.py
+++ scoreboard.py
@@ -6,22 +6,30 @@ class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
+        with open("data.txt") as data:
+            self.high_score = int(data.read())
         self.score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
         self.hideturtle()
+        self.update_scoreboard()
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", False, align=ALIGNMENT, font=FONT)
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} High Score: {self.high_score}", False, align=ALIGNMENT, font=FONT)
 
+    def reset(self):
+        if self.score > int(self.high_score):
+            self.high_score = self.score
+            with open("data.txt", mode="w") as score:
+                score.write(f"{self.high_score}")
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", False, align=ALIGNMENT, font=FONT)
+        self.update_scoreboard()
 
 
 
